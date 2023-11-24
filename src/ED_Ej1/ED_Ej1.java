d2331fddee491301026035d37ad090fd95a19a1f
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-24 20:11:48 +0100

    primer paso ejercicio1

---
+++ src/ED_Ej1/ED_Ej1.java
@@ -0,0 +1,15 @@
+package ED_Ej1;
+
+public class ED_Ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo");
+		
+		
+		
+		
+	}
+
+}
