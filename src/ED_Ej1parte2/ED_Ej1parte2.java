f902fb49cde0ef20ad44987c29eed6aaeef08326
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-24 20:43:12 +0100

    la nueva clase cantante

---
+++ src/ED_Ej1parte2/ED_Ej1parte2.java
@@ -0,0 +1,12 @@
+package ED_Ej1parte2;
+
+public class ED_Ej1parte2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("cantante");
+	}
+
+}
