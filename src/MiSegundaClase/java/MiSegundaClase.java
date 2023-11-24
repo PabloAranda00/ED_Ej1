49d9e0f043a165862ebad806edf9958b4980d25a
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-11-24 20:50:01 +0100

    nueva clase con mensaje

---
+++ src/MiSegundaClase/java/MiSegundaClase.java
@@ -0,0 +1,11 @@
+package MiSegundaClase.java;
+
+public class MiSegundaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hola");
+	}
+
+}
